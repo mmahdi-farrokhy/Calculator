a352b31ad8af3162c79b9386e40a069a87d859a1
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2022-12-24 22:42:22 +0330

    Project's final version

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -7,8 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Calculator extends Application {
     public static void main(String[] args) {
         launch(args);
